f51038e1badf2f4faf745e8c694c954810913d09
noa.kotkovski <noah.ciel@gmail.com>
2019-12-25 07:16:28 +0200

    #

--- app/src/main/java/com/example/suitedcoffee/coffeeapp/ContactUs.java
+++ app/src/main/java/com/example/suitedcoffee/coffeeapp/ContactUs.java
@@ -1,13 +1,29 @@
 package com.example.suitedcoffee.coffeeapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ContactUs extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        //register with the listener interface
+        Button contact_info_btn=(Button)findViewById(R.id.contact_us_btn);
+
+        //setting a click listener on that view
+        contact_info_btn.setOnClickListener(new View.OnClickListener(){
+            //code in this method will be executed when contact_us button is pressed
+            @Override
+            public void onClick(View view){
+
+            }
+        });
     }
 }
