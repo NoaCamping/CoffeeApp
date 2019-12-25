c4683cfdc23bbc7b69e03762264d4d6ef207311e
noa.kotkovski <noah.ciel@gmail.com>
2019-12-25 08:02:59 +0200

    #

--- app/src/main/java/com/example/suitedcoffee/coffeeapp/ContactUs.java
+++ app/src/main/java/com/example/suitedcoffee/coffeeapp/ContactUs.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class ContactUs extends AppCompatActivity {
     @Override
@@ -16,13 +21,18 @@ public class ContactUs extends AppCompatActivity {
 
         //register with the listener interface
         Button contact_info_btn=(Button)findViewById(R.id.contact_us_btn);
+        final TextView name=(TextView)findViewById(R.id.contact_us_intro_1);
+        final TextView email=(TextView)findViewById(R.id.contact_us_intro_2);
+        final TextView phone=(TextView)findViewById(R.id.contact_us_intro_3);
+        final TextView message=(TextView)findViewById(R.id.contact_us_intro_5);
 
         //setting a click listener on that view
         contact_info_btn.setOnClickListener(new View.OnClickListener(){
             //code in this method will be executed when contact_us button is pressed
             @Override
             public void onClick(View view){
-
+                //Toast.makeText(ContactUs.this, email.getText()+" ,"+phone.getText(), Toast.LENGTH_SHORT).show();
+                //Inserting messages data into database
             }
         });
     }
